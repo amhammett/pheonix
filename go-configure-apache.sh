b17f2f191d7c8e95b1dd0eeddbbf03ace4e1ca5c
Andrew Hammett <am.hammett@gmail.com>
2014-07-28 20:28:18 +1000

    ahammett: update with vars

--- go-configure-apache.sh
+++ go-configure-apache.sh
@@ -4,6 +4,8 @@ HOST=$1
 USER=$2
 KEY_FILE=$3
 
+shift 3
 
 ansible-playbook configure-apache.yml -i $HOST, \
-  --user=$USER --private-key=$KEY_FILE
+  --user=$USER --private-key=$KEY_FILE \
+  --extra-vars "$@"
